442e298e101c148b8f2b518d8ea2650b499b26fe
Victoria-Fire <vik.fire@yandex.ru>
2022-04-16 07:42:26 +0300

    Batman added

---
+++ SupermanTest/src/main/java/Superman/App.java
@@ -0,0 +1,14 @@
+package Superman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Superman!" );
+        Batman.qwerty();
+    }
+}
